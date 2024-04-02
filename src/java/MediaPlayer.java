91ceac360f1bd0346e9eda43035f2ae25b531a02
Alejandro Antelo <alejandronicolasantelo@gmail.com>
2024-04-02 13:10:53 -0300

    nueva conuslta

--- src/java/MediaPlayer.java
+++ src/java/MediaPlayer.java
@@ -11,6 +11,7 @@ public class MediaPlayer {
     * *********CONSULTAS***********
     * Es una sola media? o una lista de medias,
     * Se instancia una sola media para el mediaPlayer, o puede cambiar?
+    * Es necesario armar este metodo? O lo que realmente importa es la implementacion del adapter
     * *****************************
     */
     public void ponerPlay(){
